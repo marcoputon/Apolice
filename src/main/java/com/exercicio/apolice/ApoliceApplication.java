2f8a8653c5b708ee36c737d7c0feeef2c89cc1b2
Marco Aurelio Alves Puton <marco.puton@UDNDELL1BB5423.pampa.compasso>
2020-07-16 16:22:34 -0300

    Commit inicial

---
+++ src/main/java/com/exercicio/apolice/ApoliceApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio.apolice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApoliceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApoliceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercicio/apolice/ApoliceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.apolice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApoliceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
